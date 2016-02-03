af2e2a5922e025b9efec3a7260e9dab86ed39266
Rodolfo Mendes <Rodolfo Mendes>
2016-02-03 00:07:07 -0200

    Correlation function

--- R_Programming/week2/corr.R
+++ R_Programming/week2/corr.R
@@ -1,3 +1,22 @@
-corr <- function(directory, threshold){
+corr <- function(directory, threshold = 0){
+    correlations <- NULL
     
+    for(file in list.files(path = as.character(directory), pattern = '.csv$', full.names = TRUE)){
+        data <- read.csv(file)
+        data <- data[
+                !is.na(data$Date) & 
+                !is.na(data$sulfate) &
+                !is.na(data$nitrate) &
+                !is.na(data$ID),
+            ]
+        
+        sulfate <- data[['sulfate']]
+        nitrate <- data[['nitrate']]
+        
+        if(nrow(data) > threshold){
+            correlations <- c(correlations, cor(sulfate, nitrate))
+        }
+    }
+    
+    correlations
 }
